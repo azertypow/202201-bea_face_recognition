f4a294f15af318b65f30458d8a491fb685935c75
azertypow <azertypow@gmail.com>
2022-01-18 20:48:59 +0100

    audio implementation

---
+++ src/script/AudioControl.js
@@ -0,0 +1,37 @@
+export class AudioControl {
+    #element = document.createElement('audio')
+
+    #volume = 0
+
+    constructor() {
+        this.#element.setAttribute('src', 'public/_sound.mp3')
+        this.#element.setAttribute('autoplay', 'autoplay')
+        this.#element.loop = true
+        this.#element.play()
+
+        this.#setVolume()
+    }
+
+    /**
+     * @param {number} value between 0 et 100
+     */
+    set volume(value) {
+        if(value < 0) this.#volume      = 0
+        if(value > 100) this.#volume    = 100
+        this.#volume = value
+    }
+
+    #setVolume() {
+        const INCREMENT = .005
+
+        const volumeDown    = this.#element.volume * 100 > this.#volume
+        const volumeUp      = this.#element.volume * 100 < this.#volume
+
+        if(volumeDown   && this.#element.volume - INCREMENT > 0 )   this.#element.volume -= INCREMENT
+        if(volumeUp     && this.#element.volume + INCREMENT < 1)    this.#element.volume += INCREMENT
+
+        console.log( this.#element.volume )
+
+        window.requestAnimationFrame(() => { this.#setVolume() })
+    }
+}
--- src/script/Circle.js
+++ src/script/Circle.js
@@ -1,16 +1,17 @@
 export class Circle {
     #element    = document.createElement('div')
-    #x          = 0
-    #y          = 0
-    #width      = 50
-    #height     = 50
+    #x          = 50
+    #y          = 50
+    #width      = 5
+    #height     = 5
+    #background = "black"
 
     constructor() {
         document.body.appendChild( this.#element )
         this.#element.style.position = "fixed"
         this.#element.style.borderRadius = "100%"
-        this.#element.style.background = "black"
         this.#element.style.zIndex = "100000"
+        this.#element.style.transition = "width 250ms, height 250ms, top 250ms, left 250ms"
 
         this.x      = this.#x
         this.y      = this.#y
@@ -18,14 +19,22 @@ export class Circle {
         this.height = this.#height
     }
 
+    /**
+     * @param {'red' | 'black'} color
+     */
+    set background(color) {
+        this.#background = color
+        this.#element.style.background = this.#background
+    }
+
     set x(value) {
         this.#x = value
-        this.#element.style.left    = `${this.#x}vh`
+        this.#element.style.left    = `${this.#x}vw`
     }
 
     set y(value) {
         this.#y = value
-        this.#element.style.top    = `${this.#y}vw`
+        this.#element.style.top    = `${this.#y}vh`
     }
 
     set width(value) {
